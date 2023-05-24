112e2fdd677593a18bcdacf10fd51b7efa084fd9
Halim Choi <hchoicareer@gmail.com>
2023-05-24 17:17:52 -0400

    completed lab

---
+++ lib/migrations/versions/c83d342925b2_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: c83d342925b2
+Revises: 
+Create Date: 2023-05-24 17:08:27.942511
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c83d342925b2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
